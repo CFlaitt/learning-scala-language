2052a0bfbb0c33dad5d373457d6000f6ae10048a
Cleber Flaitt <cflaitt@gmail.com>
2023-05-10 09:23:01 -0300

    inclusao de novos objetos

---
+++ src/main/scala/br/com/learningscalalanguage/A022TryCatchFinally/TryCatchFinally_v1.scala
@@ -0,0 +1,15 @@
+package br.com.learningscalalanguage.A022TryCatchFinally
+
+import scala.io.Source
+
+object TryCatchFinally_v1 extends App {
+
+  val sourcePath = Source.fromFile("src/main/resources/dividendStocks.csv")
+
+  println("Reading file completed")
+
+  val lines = sourcePath.getLines.toList
+  lines.foreach(println)
+
+  println("Displaying lines completed")
+}
